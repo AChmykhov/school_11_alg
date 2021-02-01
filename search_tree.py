4ede189d80de2a8fa764354d12cdebcdbec37ca1
AChmykhov <alexander.chmykhov@gmail.com>
2021-02-01 10:23:27 +0300

    Complete iterate and start range

--- search_tree.py
+++ search_tree.py
@@ -37,9 +37,26 @@ class Node:
             if key < self.key:
                 return self.left.put(key, value, left, right)
 
-
     def iterate(self):
         if self.left is None and self.right is None:
-            return self
+            return [self]
         elif self.left is None:
+            return [self] + self.right.iterate()
+        elif self.right is None:
+            return self.left.iterate() + [self]
+        else:
+            return self.left.iterate() + [self] + self.right.iterate()
+
+    def range(self, start_key, end_key):
+        if start_key <= self.key <= end_key:
+            if start_key == end_key == self.key:
+                return [self]
+            elif start_key == self.key:
+                return [self] + self.right.range(start_key, end_key)
+            elif self.key == end_key:
+                return self.left.range()
+
+            else:
+        else:
+            return []
 
