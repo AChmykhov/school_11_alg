0328260bebe0212dcae2e6bd6ab4470cd2b3e300
AChmykhov <alexander.chmykhov@gmail.com>
2021-03-10 12:41:55 +0300

    replaced matmul with dotproduct, but strangely dot return matrix, not vector

--- t9_markov_chain.py
+++ t9_markov_chain.py
@@ -17,7 +17,7 @@ def train(path):
     for i in range(0, len(teach_text), 2):
         curr = pairs.index(teach_text[i:i + 2])
         if i != (len(teach_text) - 2):
-            next_curr = pairs.index(teach_text[i:i + 2])
+            next_curr = pairs.index(teach_text[i + 2:i + 4])
         else:
             next_curr = curr
         A[next_curr][curr] += 1
@@ -30,21 +30,23 @@ def train(path):
 
 def generate(P, pairs, count_pairs):
     out = ''
-    state = input("Enter first two symbols, for example 'I '")
+    state = input("Enter first two symbols, for example 'I '\n")
     if pairs.count(state) == 0:
         print("Sorry, this pair isn't known")
     else:
-        depth = int(input("Enter depth"))
+        depth = int(input("Enter depth\n"))
         for i in range(0, depth):
             out += state
             v = np.zeros(count_pairs)
             v[pairs.index(state)] = 1
-            state = np.random.choice(a=pairs, p=np.matmul(P, v))
+            print(np.dot(P, v). shape, v.shape)
+            state = np.random.choice(a=pairs, p=np.dot(P, v))
     print(out)
     print(P)
 
 
 path = "./dataset/cherchill_iron_certain_speech.txt"
 P, A, s, pairs, count_pairs = train(path)
-print(A, "1\n", P, "2\n", s)
-# generate(P, pairs, count_pairs)
+# print(A, "1\n", P, "2\n", s)
+# print(A.shape, s.shape, P.shape)
+generate(P, pairs, count_pairs)
